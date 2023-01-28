f0462f78d05ffa8c214c05ad46fdf0be63d9b439
nnapolitx <nnapolitano88@gmail.com>
2023-01-28 12:29:32 -0300

    using test file to refactor and namespace mastermind.rb code

--- mastermind.rb
+++ mastermind.rb
@@ -72,46 +72,6 @@ def generate_feedback(guess, main_answer, feedback = [])
   puts feedback.sort.join('')
 end
 
-# algo module for generating feedback on guesses
-module FeedbackAlgo
-  def copy_variables(guess, main_answer)
-    answer = main_answer.dup
-    guess = guess.split('').map(&:to_i)
-    check_answer_includes_n(guess, answer)
-  end
-
-  def check_answer_includes_n(guess, answer, feedback = [])
-    guess.each_with_index do |n, i|
-      next unless answer.include?(n)
-
-      feedback.push(check_duplicates(n, guess, answer, i))
-    end
-    puts feedback.sort.join('')
-  end
-
-  def check_duplicates(num, guess, answer, index, jdex = answer.index(num))
-    if condition_one(guess, answer, num, jdex, index)
-      guess[index] = 'N'
-    elsif condition_two(guess, answer, num, jdex, index)
-      guess[i] = 'N'
-      'O'
-    else
-      check_n_position(guess, answer, num, jdex, index)
-    end
-  end
-
-  def check_n_position(guess, answer, num, jdex, index)
-    guess[index] = 'N'
-    if num == answer[index]
-      answer[index] = 'N'
-      'X'
-    else
-      answer[jdex] = 'N'
-      'O'
-    end
-  end
-end
-
 def condition_one(guess, answer, num, index_a, index_g)
   guess.count(num) > 1 && index_a != index_g && answer.count(num) < guess.count(num)
 end
